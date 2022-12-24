f4aa0110deec7d9bd079e54609a88a62545ab2a5
Taeyooooon <eric568@naver.com>
2022-12-24 17:41:18 +0900

    ADD : Nav 레이아웃 구현

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,101 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const NAV_TOP = [
+  { id: 1, list: '고객센터', link: '/notice' },
+  { id: 2, list: '마이페이지', link: '/mypage' },
+  { id: 3, list: '로그인', link: '/signin' },
+];
+
+const NAV_MAIN = [
+  { id: 1, list: 'STYLE', link: '/style' },
+  { id: 2, list: 'SHOP', link: '/shop' },
+  { id: 3, list: 'ABOUT', link: '/about' },
+  {
+    id: 4,
+    list: <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" size="2x" />,
+  },
+];
 
 const Nav = () => {
   return (
-    <div>
-      <div>Nav Component</div>
-    </div>
+    <NavWrapper>
+      <NavTopBox>
+        <NavTopList>
+          {NAV_TOP.map(({ id, list, link }) => {
+            return (
+              <NavTopItem key={id}>
+                <Link to={link}>{list}</Link>
+              </NavTopItem>
+            );
+          })}
+        </NavTopList>
+      </NavTopBox>
+
+      <NavMainBox>
+        <NavLogoBox>ICECREAM</NavLogoBox>
+        <NavMainList>
+          {NAV_MAIN.map(({ id, list, link }) => {
+            return (
+              <NavMainItem key={id}>
+                <Link to={link}>{list}</Link>
+              </NavMainItem>
+            );
+          })}
+        </NavMainList>
+      </NavMainBox>
+    </NavWrapper>
   );
 };
 
 export default Nav;
+
+const NavWrapper = styled.nav`
+  box-shadow: 0 1px 0 0 rgb(0 0 0 / 10%);
+`;
+
+const NavTopBox = styled.div`
+  padding: 10px 40px;
+  box-shadow: 0 1px 0 0 rgb(0 0 0 / 10%);
+`;
+
+const NavTopList = styled.ul`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const NavTopItem = styled.li`
+  display: flex;
+  align-items: center;
+  color: ${props => props.theme.mainBrandGray08};
+  font-size: 12px;
+  margin-left: 24px;
+  letter-spacing: 1px;
+`;
+
+const NavMainBox = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0 40px;
+  height: 68px;
+  justify-content: space-between;
+`;
+
+const NavLogoBox = styled.div``;
+
+const NavMainList = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+const NavMainItem = styled.li`
+  font-size: 15px;
+  letter-spacing: 1px;
+  margin-right: 40px;
+
+  &:last-child {
+    margin-right: 0;
+  }
+`;
